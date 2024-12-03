467581f1cd7bb13bf9cf877cba2202d579c631ae
Jackson Rudnick <jacksonrudnick32@gmail.com>
2024-12-03 17:38:32 -0600

    Update AdminSide.java

--- src/AdminSide.java
+++ src/AdminSide.java
@@ -3,11 +3,13 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.InetAddress;
 
 public class AdminSide {
     static JPanel entryPanel;
     static JButton addEntryButton;
     private static AdminUserEntriesPanel userEntriesPanel;
+
     public static void main(String[] args) throws IOException {
         MainFrame mainFrame = new MainFrame();
         entryPanel = new JPanel();
@@ -18,22 +20,33 @@ public class AdminSide {
         userEntriesPanel = new AdminUserEntriesPanel();
         entryPanel.add(userEntriesPanel);
 
-        JPanel placeholderPanel = new JPanel();
+        mainFrame.setLayout(new BorderLayout());
+
+        mainFrame.add(entryPanel, BorderLayout.CENTER);
+
+        Panel toolBar = new Panel();
+        mainFrame.add(toolBar, BorderLayout.NORTH);
+
+        JLabel ipField = new JLabel("IP Address :: " + InetAddress.getLocalHost().getHostAddress());
+        toolBar.add(ipField);
+
+        JButton startServerButton = new JButton("Start Server");
+        startServerButton.addActionListener(e -> {
+            AdminSocket serverSocket = new AdminSocket();
+		});
+        toolBar.add(startServerButton);
+
         JButton addEntryButton = new JButton("Add Entry");
         addEntryButton.addActionListener(e -> {
             AdminAddEntry adminAddEntry = new AdminAddEntry(userEntriesPanel);
-
         });
-        placeholderPanel.add(addEntryButton);
-        entryPanel.add(placeholderPanel);
+        toolBar.add(addEntryButton);
 
         mainFrame.add(entryPanel);
         DetailedEntryPanel detailedEntryPanel = new DetailedEntryPanel();
         mainFrame.add(detailedEntryPanel);
-        userEntriesPanel.setVisible(true);
-        userEntriesPanel.repaint();
-        userEntriesPanel.revalidate();
 
+        mainFrame.revalidate();
+        mainFrame.repaint();
     }
-
 }
